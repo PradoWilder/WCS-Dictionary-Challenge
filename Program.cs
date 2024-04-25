5c8b9e12407c957652d4a1aa33d9775094b25fd6
PradoWilder <p.o.maisonneuve@gmail.com>
2024-04-25 15:45:08 +0200

    Commit for the Dictionary Challenge work

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace DictionnaryChallenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Création du dictionnaire
+            Dictionary<char, string> dictionary = new Dictionary<char, string>();
+
+            // Ajout des éléments 'a', 'b' et 'c' dans le dictionnaire
+            dictionary.Add('a', "Comme : Alphabet ou Abracadabra");
+            dictionary.Add('b', "Comme : Beta ou Big");
+            dictionary.Add('c', "Comme : Charlie ou Chance");
+
+            // Affichage du dictionnaire avant la suppression - (clé, Valeur)
+            Console.WriteLine("Dictionnaire avant suppression de la lettre 'c':");
+            foreach (var kvp in dictionary)
+            {
+                Console.WriteLine($"Clé : {kvp.Key}, Valeur : {kvp.Value}");
+            }
+
+            // Suppression de la lettre 'c'
+            dictionary.Remove('c');
+
+            // Affichage du dictionnaire après la suppression - (Clé, Valeur)
+            Console.WriteLine("\nDictionnaire après suppression de la lettre 'c':");
+            foreach (var kvp in dictionary)
+            {
+                Console.WriteLine($"Clé : {kvp.Key}, Valeur : {kvp.Value}");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# WCS-Dictionary-Challenge
